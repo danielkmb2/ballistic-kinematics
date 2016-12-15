327ca7b01f57a284f1567e351d528234bd692c90
Mosquito <danielkmb2@gmail.com>
2016-12-15 22:18:53 +0100

    simple improvements in the test gun

--- Assets/_Demo/scripts/SimpleGun.cs
+++ Assets/_Demo/scripts/SimpleGun.cs
@@ -2,21 +2,33 @@ using UnityEngine;
 
 public class SimpleGun : MonoBehaviour {
 
+	public float fireRate = 0.05f;
 	public float rigidBulletPower = 30f;
 
+	public GameObject shotEffects;
 	public GameObject rigidBullet;
 	public GameObject bullet;
 	public Transform spawnpoint;
 	public Camera cam;
 
+	private float lastShotTime = 0f;
+
 	void Update() {
-		if (Input.GetKey("mouse 0")) {
-			GameObject bullet = GetRigidBullet(spawnpoint.transform.position, cam.transform.rotation);
-			ShotRigidBullet(bullet);
-		}
-		if (Input.GetKey("mouse 1")) {
-			GameObject bullet = GetBullet(spawnpoint.transform.position, cam.transform.rotation);
-			ShotBullet(bullet);
+
+
+		if (lastShotTime + fireRate < Time.time) {
+
+			if (Input.GetKey("mouse 0")) {
+				GameObject bullet = GetRigidBullet(spawnpoint.transform.position, cam.transform.rotation);
+				ShotRigidBullet(bullet);
+				lastShotTime = Time.time;
+			}
+			if (Input.GetKey("mouse 1")) {
+				GameObject bullet = GetBullet(spawnpoint.transform.position, cam.transform.rotation);
+				ShotBullet(bullet);
+				lastShotTime = Time.time;
+			}
+
 		}
 
 	}
@@ -27,7 +39,7 @@ public class SimpleGun : MonoBehaviour {
 	}
 
 	private void ShotRigidBullet(GameObject bullet) {
-		bullet.GetComponent<Rigidbody>().velocity = spawnpoint.forward * rigidBulletPower;
+		bullet.GetComponent<Rigidbody>().velocity = spawnpoint.up * rigidBulletPower;
 	}
 
 	private GameObject GetBullet(Vector3 position, Quaternion rotation) {
@@ -36,6 +48,10 @@ public class SimpleGun : MonoBehaviour {
 	}
 
 	private void ShotBullet(GameObject bullet) {
-		bullet.GetComponent<BulletBehaviour>().StartTrajectory(spawnpoint.forward * rigidBulletPower);
+		if (shotEffects != null) {
+			Instantiate(shotEffects, spawnpoint.position, spawnpoint.rotation);
+		}
+
+		bullet.GetComponent<BulletBehaviour>().StartTrajectory(spawnpoint.up * rigidBulletPower);
 	}
 }
