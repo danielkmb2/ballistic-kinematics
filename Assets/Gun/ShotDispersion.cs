1d6c4f610ab22844ff57d17482fe013a1a484819
Mosquito <danielkmb2@gmail.com>
2016-12-17 20:04:20 +0100

    lets not divide by zero

--- Assets/Gun/ShotDispersion.cs
+++ Assets/Gun/ShotDispersion.cs
@@ -8,6 +8,12 @@ public class ShotDispersion {
 	public float dispersionTime = 2f;
 
 	public float getDispersionRate(float shootingTime) {
+
+		if (dispersionTime == 0) {
+			// avoid get divided by zero and die
+			return maxDispersion;
+		}
+
 		float normalizedDispersionTime = Mathf.Clamp(shootingTime, 0, dispersionTime) / dispersionTime;
 		float currentDispersion = scaleFunction.Evaluate(normalizedDispersionTime) * maxDispersion;
 		return currentDispersion;
